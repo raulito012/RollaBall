f889adc334148601d52b3a30d726ff66d41285ce
Jose Quezada <raulquezada729@gmail.com>
2022-02-27 17:09:16 -0400

    Subiendo

---
+++ Assets/Scripts/JugadorController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class JugadorController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame 
+
+    public float  velocidad;
+
+    private int contador;
+
+    public Text textoContador, textoGanar;
+
+    
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        contador = 0;
+
+        setTextoContador();
+
+        textoGanar.text= "";
+        
+    }
+
+    void FixedUpdate()
+    {
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movimientoH, 0.0f, movimientoV);
+
+        rb.AddForce(movimiento * velocidad);
+
+    }
+
+
+    void OnTriggerEnter(Collider other){
+
+         if (other.gameObject.CompareTag ("Coleccionable"))
+         {
+              other.gameObject.SetActive(false);
+              contador = contador + 1;
+              setTextoContador();
+
+         }
+    }
+
+
+    void setTextoContador(){
+
+        textoContador.text = "Contador: " + contador.ToString();
+
+        if(contador >= 12){
+            textoGanar.text = "Ganaste!!!";
+        }
+
+       
+
+
+        
+    }
+
+
+
+}
